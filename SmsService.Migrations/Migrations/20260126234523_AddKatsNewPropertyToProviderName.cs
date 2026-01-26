2818e406a36bfaddcb60e10bd9d28206d7ccb24b
Katrina <katrina.mccallum@tixtrack.com>
2026-01-26 15:59:19 -0800

    migration ahead of model change

---
+++ SmsService.Migrations/Migrations/20260126234523_AddKatsNewPropertyToProviderName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmsService.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddKatsNewPropertyToProviderName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "KatsNewProperty",
+                table: "ProviderNames",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KatsNewProperty",
+                table: "ProviderNames");
+        }
+    }
+}
