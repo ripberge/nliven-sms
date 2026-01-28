a4c558763f038e18793337dd6da9638f986f390d
Katrina <katrina.mccallum@tixtrack.com>
2026-01-28 11:25:14 -0800

    code review - putting stringEnumConverted into program.cs

--- SmsService.Domain/Enums/ConsentSource.cs
+++ SmsService.Domain/Enums/ConsentSource.cs
@@ -1,9 +1,5 @@
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-
 namespace SmsService.Domain.Enums;
 
-[JsonConverter(typeof(StringEnumConverter))]
 public enum ConsentSource
 {
     Checkout = 1,
